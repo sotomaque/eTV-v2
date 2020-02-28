6772de8cd0d46e799d8ffd2aecc7b4906806d382
sotomaque <sotomaque@gmail.com>
2020-02-28 10:33:23 -0800

    Updates

---
+++ precache-manifest.7fbfb4d7b092b624ee5321c2a0bc3dc5.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "814eb8797ef660229ab024e6ba8e0a1a",
+    "url": "/eTV-v2/index.html"
+  },
+  {
+    "revision": "420d05a0c2e367f8f30d",
+    "url": "/eTV-v2/static/css/main.a0c559b9.chunk.css"
+  },
+  {
+    "revision": "add488175d04cc77c753",
+    "url": "/eTV-v2/static/js/2.5347ad6c.chunk.js"
+  },
+  {
+    "revision": "60f197fdb09c5629dbe9aad6dcccb06a",
+    "url": "/eTV-v2/static/js/2.5347ad6c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "420d05a0c2e367f8f30d",
+    "url": "/eTV-v2/static/js/main.0d63d56e.chunk.js"
+  },
+  {
+    "revision": "c0d2ee130fb7174b03f6",
+    "url": "/eTV-v2/static/js/runtime-main.c55dd9c2.js"
+  },
+  {
+    "revision": "cc93a967d58329d2fd189e3476d5bb51",
+    "url": "/eTV-v2/static/media/avatar.cc93a967.jpg"
+  },
+  {
+    "revision": "843781834b1db2908f08d921c6460c81",
+    "url": "/eTV-v2/static/media/bg2.84378183.jpg"
+  },
+  {
+    "revision": "e76de24b6d8ab5e920bb4d49241ad15f",
+    "url": "/eTV-v2/static/media/bg3.e76de24b.jpg"
+  },
+  {
+    "revision": "199e9ec14308832c6a8a49bd96c1fde6",
+    "url": "/eTV-v2/static/media/bg4.199e9ec1.jpg"
+  },
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "/eTV-v2/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "788e3d36e8c4d4120daeee6e57e7e85f",
+    "url": "/eTV-v2/static/media/card-profile2-square.788e3d36.jpg"
+  },
+  {
+    "revision": "58a713c73886bac25ac6aedcbb13de5a",
+    "url": "/eTV-v2/static/media/city.58a713c7.jpg"
+  },
+  {
+    "revision": "0d005146698109b71c581179f033726a",
+    "url": "/eTV-v2/static/media/clem-onojegaw.0d005146.jpg"
+  },
+  {
+    "revision": "f8f6718ac53d7b470b30c4a8926c48b1",
+    "url": "/eTV-v2/static/media/clem-onojeghuo.f8f6718a.jpg"
+  },
+  {
+    "revision": "3a7ae9b9f8081f8474277f79b818a7b0",
+    "url": "/eTV-v2/static/media/cynthia-del-rio.3a7ae9b9.jpg"
+  },
+  {
+    "revision": "5a0e395b3b76565583339619ca3110f4",
+    "url": "/eTV-v2/static/media/kendall.5a0e395b.jpg"
+  },
+  {
+    "revision": "df8fd3efcd662b64b44de07f351c838b",
+    "url": "/eTV-v2/static/media/landing-bg.df8fd3ef.jpg"
+  },
+  {
+    "revision": "101b9dc3484755250ff3de71d22af8e1",
+    "url": "/eTV-v2/static/media/marc.101b9dc3.jpg"
+  },
+  {
+    "revision": "a30894f58c2a76c460f3fd8c84d57752",
+    "url": "/eTV-v2/static/media/mariya-georgieva.a30894f5.jpg"
+  },
+  {
+    "revision": "d9bcce95b65b233009421ed21c510bc7",
+    "url": "/eTV-v2/static/media/olu-eletu.d9bcce95.jpg"
+  }
+]);
